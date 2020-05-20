fb1ff2a0213d707b45d8c10d95cf81dea5eaddc0
seban3t <64905774+seban3t@users.noreply.github.com>
2020-05-20 22:12:57 +0700

    update line of codes

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,23 @@
 function myFunction(x) {
-		var ul = document.getElementById('sidenav');
-		x.classList.toggle("change");
-		ul.classList.toggle("sidenav");
-	}
+	var ul = document.getElementById('sidenav');
+	x.classList.toggle("change");
+	ul.classList.toggle("sidenav");
+}
+
+$('.scrolled').click(function() {
+$('html, body').animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top-90
+}, 750);
+ return false;
+});
+
+function onElementFocused(e)
+{
+    if (e && e.target)
+        document.activeElement = e.target == document ? null : e.target;
+} 
+
+if (document.addEventListener) 
+    document.addEventListener("focus", onElementFocused, true);
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
